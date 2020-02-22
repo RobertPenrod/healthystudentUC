ec63e3793b37c40c2b5e5d71119f582f103927b9
Samantha Bowman <bsammilee25@gmail.com>
2020-02-22 14:46:42 -0500

    Form Start

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Form} from 'react-bootstrap'
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <div id = "form"></div>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
     </div>
   );
 }
-
 export default App;
