061ba35eb56d2996a2f437476c6627794c40f37c
Robert Penrod <robertpenrod37@gmail.com>
2020-02-22 16:49:57 -0500

    List Changes

--- src/Components/ShoppingList/ShoppingList.js
+++ src/Components/ShoppingList/ShoppingList.js
@@ -3,42 +3,6 @@ import ListGroup from 'react-bootstrap/ListGroup';
 
 class ShoppingList extends React.Component
 {
-    constructor() {
-        super();
-        this.state = {
-          width:  800,
-          height: 182
-        }
-      }
-    
-      /**
-       * Calculate & Update state of new dimensions
-       */
-      updateDimensions() {
-        if(window.innerWidth < 500) {
-          this.setState({ width: 450, height: 102 });
-        } else {
-          let update_width  = window.innerWidth-100;
-          let update_height = Math.round(update_width/4.4);
-          this.setState({ width: update_width, height: update_height });
-        }
-      }
-    
-      /**
-       * Add event listener
-       */
-      componentDidMount() {
-        this.updateDimensions();
-        window.addEventListener("resize", this.updateDimensions.bind(this));
-      }
-    
-      /**
-       * Remove event listener
-       */
-      componentWillUnmount() {
-        window.removeEventListener("resize", this.updateDimensions.bind(this));
-      }
-
     render()
     {
         const listStyle = {
@@ -48,7 +12,7 @@ class ShoppingList extends React.Component
         }
 
         return (
-            <>
+            <div>
                 <h2>Shopping List</h2>
                 <ListGroup style={listStyle}>
                     <ListGroup.Item>Grocery Item...</ListGroup.Item>
@@ -72,7 +36,7 @@ class ShoppingList extends React.Component
                     <ListGroup.Item>Grocery Item...</ListGroup.Item>
                     <ListGroup.Item>Grocery Item...</ListGroup.Item>
                 </ListGroup>
-            </>
+            </div>
         )
     }
 }
