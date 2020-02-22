29a1ff52b04ab08aa3a781da922e4a5c739adebe
Samantha Bowman <bsammilee25@gmail.com>
2020-02-22 15:07:47 -0500

    Top

--- src/App.js
+++ src/App.js
@@ -2,25 +2,30 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Form} from 'react-bootstrap'
+import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
     <div className="App">
-      <div id = "form"></div>
+      <div id = "form">
         <Form>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
+          <Form.Group as={Row} controlId="formPlaintextHouseholdNumber">
+              <Col sm="10" md={{ span: 4, offset: 4 }}>
+                <Row>
+                  <Form.Control type="householdnumber" placeholder="HouseholdNumber" />
+                </Row>
+              </Col>
+              <Col sm="1">
+                  <Button variant="primary" type="submit">
+                    Submit
+                  </Button>
+              </Col>
           </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
         </Form>
+      </div>
     </div>
   );
 }
