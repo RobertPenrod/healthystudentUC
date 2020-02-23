3f31010f3d3f1126701afadd1386852b7e846165
Samantha Bowman <bsammilee25@gmail.com>
2020-02-22 19:51:13 -0500

    revert

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import { Navbar, FormControl} from 'react-bootstrap';
 
 import ShoppingList from './Components/ShoppingList/ShoppingList';
 
-//test
 function App() {
   const state = {
     dataHorizontal: {
